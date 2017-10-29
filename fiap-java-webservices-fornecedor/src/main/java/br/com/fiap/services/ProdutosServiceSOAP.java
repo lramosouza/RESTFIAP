e223f82afb7ffc8ea39622f50cc0c6ac26160147
user <user@192.168.0.14>
2017-10-29 15:57:55 -0200

    Set na lista de produtos

--- fiap-java-webservices-fornecedor/src/main/java/br/com/fiap/services/ProdutosServiceSOAP.java
+++ fiap-java-webservices-fornecedor/src/main/java/br/com/fiap/services/ProdutosServiceSOAP.java
@@ -25,26 +25,45 @@ public class ProdutosServiceSOAP {
 		produtos = new ArrayList<Produto>();
         
         Produto produto1 = new Produto();
-        produto1.setCodProduto(1);
-        produto1.setDescricao("Teste produto 1");
-        produto1.setQtdEstoque(10);
-        produto1.setValor(Util.toBigDecimal(100));
+        produto1.setCodProduto(0001);
+        produto1.setDescricao("DVD-Player");
+        produto1.setQtdEstoque(100);
+        produto1.setValor(Util.toBigDecimal(350));
+        produto1.setProdutoDisponivel(true);
 
         Produto produto2 = new Produto();
-        produto2.setCodProduto(2);
-        produto2.setDescricao("Teste produto 2");
-        produto2.setQtdEstoque(20);
-        produto2.setValor(Util.toBigDecimal(200));
+        produto2.setCodProduto(0002);
+        produto2.setDescricao("Galaxy Note 8");
+        produto2.setQtdEstoque(120);
+        produto2.setValor(Util.toBigDecimal(5200));
+        produto2.setProdutoDisponivel(true);
 
         Produto produto3 = new Produto();
-        produto3.setCodProduto(3);
-        produto3.setDescricao("Teste produto 3");
+        produto3.setCodProduto(0003);
+        produto3.setDescricao("JBL Flip 4 Speaker");
         produto3.setQtdEstoque(30);
-        produto3.setValor(Util.toBigDecimal(300));
+        produto3.setValor(Util.toBigDecimal(500));
+        produto3.setProdutoDisponivel(true);
+        
+        Produto produto4 = new Produto();
+        produto4.setCodProduto(0004);
+        produto4.setDescricao("Beats Headphone");
+        produto4.setQtdEstoque(40);
+        produto4.setValor(Util.toBigDecimal(850));
+        produto4.setProdutoDisponivel(true);
+        
+        Produto produto5 = new Produto();
+        produto5.setCodProduto(0005);
+        produto5.setDescricao("iPhone 7 Plus 64 GB");
+        produto5.setQtdEstoque(0);
+        produto5.setValor(Util.toBigDecimal(3500));
+        produto5.setProdutoDisponivel(false);
         
         produtos.add(produto1);
         produtos.add(produto2);
         produtos.add(produto3);
+        produtos.add(produto4);
+        produtos.add(produto5);
     }
 	
 	
@@ -54,7 +73,7 @@ public class ProdutosServiceSOAP {
 				
 		try {
 			if (isAutenticado(username, password)){
-				System.out.println(produtos.toString());
+				System.out.println(produtos);
 				return produtos;
 			}else{
 				throw new ProdutoException("Usuário não possui permissão para consultar produtos");
