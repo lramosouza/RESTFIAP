75070868d40d34e4821c4b63ea9de4f1bafa272c
Ismael Souza <ismaelsmaria@gmail.com>
2017-11-02 18:39:14 -0200

    Desenv-pedido

---
+++ fiap-java-webservices-fornecedor/src/main/java/br/com/fiap/services/jaxws/ProcessarPedido.java
@@ -0,0 +1,61 @@
+
+package br.com.fiap.services.jaxws;
+
+import java.util.List;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+import br.com.fiap.domain.Pedido;
+
+@XmlRootElement(name = "processarPedido", namespace = "http://services.fiap.com.br/")
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "processarPedido", namespace = "http://services.fiap.com.br/", propOrder = {
+    "cpfCnpj",
+    "pedido"
+})
+public class ProcessarPedido {
+
+    @XmlElement(name = "cpfCnpj", namespace = "")
+    private String cpfCnpj;
+    @XmlElement(name = "pedido", namespace = "")
+    private List<Pedido> pedido;
+
+    /**
+     * 
+     * @return
+     *     returns String
+     */
+    public String getCpfCnpj() {
+        return this.cpfCnpj;
+    }
+
+    /**
+     * 
+     * @param cpfCnpj
+     *     the value for the cpfCnpj property
+     */
+    public void setCpfCnpj(String cpfCnpj) {
+        this.cpfCnpj = cpfCnpj;
+    }
+
+    /**
+     * 
+     * @return
+     *     returns List<Pedido>
+     */
+    public List<Pedido> getPedido() {
+        return this.pedido;
+    }
+
+    /**
+     * 
+     * @param pedido
+     *     the value for the pedido property
+     */
+    public void setPedido(List<Pedido> pedido) {
+        this.pedido = pedido;
+    }
+
+}
--- fiap-java-webservices-fornecedor/src/main/java/br/com/fiap/util/Util.java
+++ fiap-java-webservices-fornecedor/src/main/java/br/com/fiap/util/Util.java
@@ -1,12 +1,64 @@
 package br.com.fiap.util;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.com.fiap.domain.Produto;
 
 public class Util {
 	
 	public static BigDecimal toBigDecimal(int valor){
 
 		return new BigDecimal(valor);
+	}
+	
+	public static List<Produto> listaProdutos(){
+		
+		List<Produto> listaProdutos =  new ArrayList<Produto>();
+        
+        Produto produto1 = new Produto();
+        produto1.setCodProduto(0001);
+        produto1.setDescricao("DVD-Player");
+        produto1.setQtdEstoque(100);
+        produto1.setValor(Util.toBigDecimal(350));
+        produto1.setProdutoDisponivel(true);
+
+        Produto produto2 = new Produto();
+        produto2.setCodProduto(0002);
+        produto2.setDescricao("Galaxy Note 8");
+        produto2.setQtdEstoque(120);
+        produto2.setValor(Util.toBigDecimal(5200));
+        produto2.setProdutoDisponivel(true);
+
+        Produto produto3 = new Produto();
+        produto3.setCodProduto(0003);
+        produto3.setDescricao("JBL Flip 4 Speaker");
+        produto3.setQtdEstoque(30);
+        produto3.setValor(Util.toBigDecimal(500));
+        produto3.setProdutoDisponivel(true);
+        
+        Produto produto4 = new Produto();
+        produto4.setCodProduto(0004);
+        produto4.setDescricao("Beats Headphone");
+        produto4.setQtdEstoque(40);
+        produto4.setValor(Util.toBigDecimal(850));
+        produto4.setProdutoDisponivel(true);
+        
+        Produto produto5 = new Produto();
+        produto5.setCodProduto(0005);
+        produto5.setDescricao("iPhone 7 Plus 64 GB");
+        produto5.setQtdEstoque(0);
+        produto5.setValor(Util.toBigDecimal(3500));
+        produto5.setProdutoDisponivel(false);
+        
+        listaProdutos.add(produto1);
+        listaProdutos.add(produto2);
+        listaProdutos.add(produto3);
+        listaProdutos.add(produto4);
+        listaProdutos.add(produto5);
+        
+        return listaProdutos;
 	}
 
 }
